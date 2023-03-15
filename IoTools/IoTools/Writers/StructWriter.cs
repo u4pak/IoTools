0cf3b5a44565d71fa04033ab11424912faec7a82
Anker <106858053+OngAnker@users.noreply.github.com>
2023-03-14 19:55:53 -0500

    initial commit

---
+++ IoTools/IoTools/Writers/StructWriter.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace IoTools.Writers;
+
+public class StructWriter : BinaryWriter
+{
+    public StructWriter(Stream stream) : base(stream) {}
+
+    public unsafe void WriteStruct<T>(T data)
+    {
+        int size = Unsafe.SizeOf<T>();
+        var buffer = new byte[size];
+        Unsafe.WriteUnaligned(ref buffer[0], data);
+        Write(buffer);
+    }
+}
